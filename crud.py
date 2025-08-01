e52d3a96104dea559999f47e073d57d88f761ab2
NGWA Solange Binui <ngwasolange12@gmail.com>
2025-08-01 05:21:42 +0100

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,3 +1,7 @@
+from sqlalchemy.orm import Session
+import pandas as pd
+import models
+
 def bulk_insert_donations(db: Session, df: pd.DataFrame):
     for _, row in df.iterrows():
         donation = models.BloodDonation(
